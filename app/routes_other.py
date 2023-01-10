b453a4f311a7e9e0a434e3c7da5b989f4adb61dc
P0010 <ap8255370@gmail.com>
2023-01-10 23:31:07 +0500

    sitemap_02

--- app/routes_other.py
+++ app/routes_other.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, request, send_from_directory
 
 from app import app
 
 
-@app.route('/sitemap/')
-def sitemap():
-    return render_template('sitemap.xml')
+@app.route('/robots.txt')
+@app.route('/sitemap.xml')
+def static_from_root():
+    return send_from_directory(app.static_folder, request.path[1:]) 
